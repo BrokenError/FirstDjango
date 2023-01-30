64c1b1f39967b6097e642266c3fe36b2aed8dc99
Maxim Logvinov <vvglvv2@gmail.com>
2023-01-31 02:12:34 +0300

    Add new urls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls), # http://127.0.0.1:8000/andmin/
-    path('women/', include('women.urls')),
+    path('', include('women.urls')),
 ]
--- mysite/women/views.py
+++ mysite/women/views.py
@@ -6,6 +6,9 @@ def index(request):
     return HttpResponse("Страница приложения women.")
 
 
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям.</h1>")
+def categories(request, catid):
+    return HttpResponse(f"<h1>Статьи по категориям.</h1><p>{catid}</p>")
 # Create your views here.
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
