ca886c648666c34e8c44df0834e755489c04c8ed
Maxim Logvinov <vvglvv2@gmail.com>
2023-02-02 23:40:27 +0300

    Add static and new dezign pages

--- mysite/women/urls.py
+++ mysite/women/urls.py
@@ -5,6 +5,9 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
-    path('cats/<slug:catid>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    # path('cats/<slug:catid>/', categories),
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
 ]
