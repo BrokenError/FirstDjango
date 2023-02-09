a0b238d9969c63a73c001132a02a41631bf95e01
Maxim Logvinov <vvglvv2@gmail.com>
2023-02-10 00:00:08 +0300

    Add new singer

--- mysite/women/admin.py
+++ mysite/women/admin.py
@@ -6,6 +6,13 @@ class WomenAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
 
 
-admin.site.register(Women)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
