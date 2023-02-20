e1be021c569b254a1da339504cc9ed745279640c
Maxim Logvinov <vvglvv2@gmail.com>
2023-02-20 23:55:53 +0300

    Сonnecting Django Debug Toolbar

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,14 +18,19 @@ from django.conf.urls.static import static
 from women.views import *
 from django.contrib import admin
 from django.urls import path, include
-
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls), # http://127.0.0.1:8000/admin/
     path('', include('women.urls')),
+
 ]
 
 if settings.DEBUG:
+    urlpatterns = [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ] + urlpatterns
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
